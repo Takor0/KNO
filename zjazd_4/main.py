cee315876edfc3610bf5002be29e4797e6bdeadd
Tak Or <86683044+Takor0@users.noreply.github.com>
2025-12-07 09:04:07 +0100

    Lab 4 (#7)
    * copied zjazd_3 model
    
    * added norm_layer
    
    * added norm_layer
    
    * added RESULTS.MD
    
    * fixed RESULTS.MD
    
    * fixed RESULTS.MD
    
    * tweaked RESULTS.MD
    
    * handle keras_tuner
    
    * added remote machine tuner results

---
+++ zjazd_4/main.py
@@ -0,0 +1,204 @@
+from pathlib import Path
+
+import keras_tuner as kt
+import numpy as np
+import pandas as pd
+import argparse
+import tensorflow as tf
+from keras import Sequential
+from keras.callbacks import EarlyStopping
+
+from keras.src import layers
+from keras.src.optimizers import Adam
+from sklearn.metrics import confusion_matrix
+
+from sklearn.model_selection import train_test_split
+from matplotlib.offsetbox import AnchoredText
+
+
+normalization_layer = None
+
+
+def plot_loss(history, batch_size, epochs, learning_rate, loc="upper right"):
+    import matplotlib.pyplot as plt
+
+    for key, label in [
+        ("accuracy", "Accuracy"),
+        ("loss", "Loss"),
+    ]:
+        savepath = f"{key}_curve.png"
+
+        fig, ax = plt.subplots(constrained_layout=True)
+
+        ax.plot(history[key], label=label)
+
+        ax.set_ylim(0, 2)
+        ax.set_xlabel("Epoch")
+        ax.set_ylabel(label)
+        ax.legend()
+        ax.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.5)
+
+        text = f"batch_size={batch_size}\nlr={learning_rate}\nepochs={epochs}"
+        at = AnchoredText(
+            text, loc=loc, pad=0.3, borderpad=0.4, prop={"family": "monospace"}
+        )
+        at.patch.set_facecolor("white")
+        at.patch.set_alpha(0.85)
+        at.patch.set_edgecolor("gray")
+        ax.add_artist(at)
+
+        fig.savefig(savepath, dpi=150)
+        plt.close(fig)
+
+
+def get_data():
+    df = pd.read_csv("data/wine.csv")
+    class_hot_one = pd.get_dummies(df["class"]).astype(int)
+    df = df.drop(columns=["class"])
+    df = df.sample(frac=1, random_state=42)
+    class_hot_one = class_hot_one.loc[df.index]
+    X_train, X_test, y_train, y_test = train_test_split(
+        df, class_hot_one, test_size=0.2, random_state=42
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def model_builder(hp):
+    model = Sequential()
+    model.add(normalization_layer)
+
+    hp_units = hp.Int("units", min_value=1, max_value=512, step=32)
+    model.add(layers.Dense(units=hp_units, activation="relu", name="hidden_dense"))
+
+    hp_rates = hp.Float("dropout", min_value=0, max_value=0.4, step=0.01)
+    model.add(layers.Dropout(hp_rates, name="hidden_dropout"))
+
+    model.add(
+        layers.Dense(3, activation="softmax", name="output"),
+    )
+
+    hp_learning_rate = hp.Choice(
+        "learning_rate", values=[1e-1, 1e-2, 1e-3, 1e-4]
+    )
+
+    model.compile(
+        optimizer=Adam(learning_rate=hp_learning_rate),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+def get_model(
+    X_train,
+    y_train,
+    recalculate=True,
+    batch_size=32,
+    epochs=10,
+    learning_rate=0.001,
+):
+    model_path = Path(f"model.keras")
+    if not model_path.exists() or recalculate:
+        tuner = kt.Hyperband(
+            model_builder,
+            objective="val_accuracy",
+            max_epochs=epochs,
+            factor=3,
+            directory="tuner_results",
+            project_name="wines",
+        )
+
+        early_stopping_callback = EarlyStopping(
+            monitor='val_accuracy',
+            mode='max',
+            baseline=1.0,
+            patience=0,
+            verbose=1
+        )
+
+        tuner.search(
+            X_train,
+            y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_split=0.2,
+            callbacks=[early_stopping_callback]
+        )
+        tuner.results_summary()
+        best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+        model = tuner.hypermodel.build(best_hps)
+        fit_result = model.fit(
+            X_train, y_train, epochs=epochs, validation_split=0.2
+        )
+
+        plot_loss(
+            fit_result.history,
+            batch_size=batch_size,
+            epochs=epochs,
+            learning_rate=learning_rate,
+        )
+        model.summary()
+        model.save(model_path)
+    else:
+        return tf.keras.models.load_model(model_path)
+
+    return model
+
+
+def main(arguments):
+    X_train, X_test, y_train, y_test = get_data()
+
+    adapt_data = np.array(X_train)
+    norm_layer = layers.Normalization()
+    norm_layer.adapt(adapt_data)
+    global normalization_layer
+    normalization_layer = norm_layer
+
+    model = get_model(
+        X_train=X_train,
+        y_train=y_train,
+        recalculate=True,
+        batch_size=32,
+        epochs=10,
+        learning_rate=0.001,
+    )
+
+    x = np.array([list(arguments.__dict__.values())])
+    probs = model.predict(x, verbose=0)[0]
+    print("predicted class: ", np.argmax(probs) + 1)
+
+    y_pred = []
+    y_true = []
+
+    for x_test, (_, y) in zip(X_test.itertuples(), y_test.iterrows()):
+        x_test_list = list(x_test)
+        x_test_list.pop(0)
+        test_in = np.array([list(x_test_list)])
+        probs = model.predict(test_in, verbose=0)[0]
+
+        y_pred.append(np.argmax(probs))
+        y_true.append(np.argmax(list(y)))
+
+    # confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    print(cm)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--min-alcohol", type=float, default=0.0)
+    parser.add_argument("--alcohol", type=float, default=0.0)
+    parser.add_argument("--ash", type=float, default=0.0)
+    parser.add_argument("--alcalinity_of_ash", type=float, default=0.0)
+    parser.add_argument("--magnesium", type=float, default=0.0)
+    parser.add_argument("--total_phenols", type=float, default=0.0)
+    parser.add_argument("--flavanoids", type=float, default=0.0)
+    parser.add_argument("--nonflavanoid_phenols", type=float, default=0.0)
+    parser.add_argument("--proanthocyanins", type=float, default=0.0)
+    parser.add_argument("--color_intensity", type=float, default=0.0)
+    parser.add_argument("--hue", type=float, default=0.0)
+    parser.add_argument("--od_diluted_wines", type=float, default=0.0)
+    parser.add_argument("--proline", type=float, default=0.0)
+
+    main(parser.parse_args())
